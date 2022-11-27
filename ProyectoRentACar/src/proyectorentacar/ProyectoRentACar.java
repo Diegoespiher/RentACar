7c22f0c624c9149f108107b095e095bbe3c019d4
Diegoespiher <diegoespinach12@gmail.com>
2022-11-26 23:13:09 -0600

    Cliente

--- ProyectoRentACar/src/proyectorentacar/ProyectoRentACar.java
+++ ProyectoRentACar/src/proyectorentacar/ProyectoRentACar.java
@@ -13,7 +13,31 @@ public class ProyectoRentACar {
    
     public static void main(String[] args) {
         //Main
+        listaCliente laLista = new listaCliente();
+        laLista.inserta(new cliente(22, "Juan", "Juan", "Juan", "Juan"));
+        laLista.inserta(new cliente(11, "Juan", "Juan", "Juan", "Juan"));
+        laLista.inserta(new cliente(15, "Juan", "Juan", "Juan", "Juan"));
+        laLista.inserta(new cliente(66, "Juan", "Juan", "Juan", "Juan"));
+        laLista.inserta(new cliente(75, "Juan", "Juan", "Juan", "Juan"));
+       
+
+        // System.out.println(laLista.extrae(30).getNombre());
+        // laLista.elimina(5);
+        // System.out.println("Existe? "+laLista.existe(20));
+        System.out.println(laLista);
+
+        System.out.println("-------Modificada------");
+
+        laLista.modifica(new cliente(15, "Didier", "uno", "uno", "uno"));
+        // laLista.modifica(new Persona(15, 65));
+        System.out.println(laLista);
+        System.out.println("-------Elimina------");
+        //laLista.elimina(5);
+        //System.out.println(laLista);
+        //laLista.extrae(5);
+        System.out.println(laLista.extrae(75));
         
     }
-    
-}
+        
+        
+    }
---
+++ ProyectoRentACar/src/proyectorentacar/alquilar.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectorentacar;
+
+/**
+ *
+ * @author Luis Diego
+ */
+public class alquilar {
+    //private vehiculo dato;
+    //private cliente dato;
+    ///private int dias;
+    //private int año;
+    //private int mes;
+    //private int dia;
+    //private String marca;
+    //private String modelo;
+    //private String añoVehiculo;
+}
